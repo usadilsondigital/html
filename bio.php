a01750d12442bd6e80aa5ad778056b2a7e60fe79
usadilsondigital <usadilsondigital@gmail.com>
2023-12-01 20:06:10 -0600

    adding z index 2 to menu

--- bio.php
+++ bio.php
@@ -59,54 +59,22 @@
         <div id="DivMiddleBio"></div>
 
         <div id="DivTextBio">
-            <p class="p1">
-                STUART M. ALDERMAN ARCHITECT</p>
-
-            <p class="p1">
-                HAS BEEN DESIGNING AND HONING THE CRAFT AND ART OF ARCHITECTURE </p>
-
-            <p class="p1">
-                SINCE HE WAS ABOUT 10 YEARS OLD. HIS MOTHER WAS AN ARTIST AND HE</p>
-
-            <p class="p1">
-                SPENT MUCH OF HIS TIME DRAWING, PAINTING AND CREATING AT HOME AND IN</p>
-
-            <p class="p2">
-                ART CLASSES.</p>
-
-            <p class="p3">
-                HE SPENT A SUMMER WHILE IN HIGH SCHOOL AT CORNELL UNIVERSITIES' </p>
-
-            <p class="p3">
-                SUMMER PROGRAM IN ARCHITECTURE AND WENT ON TO GRADUATE FROM THE</p>
-
-            <p class="p1">
-                SYRACUSE UNIVERSITY SCHOOL OF ARCHITECTURE</p>
-
-            <p class="p3">
-                AFTER SEVERAL YEARS OF WORK IN DALLAS, TEXAS HE MOVED HIS FAMILY TO</p>
-
-            <p class="p3">
-                UPSTATE NEW YORK AND HAD A CAREER AS THE PRINCIPAL DESIGNER FOR A</p>
-
-            <p class="p1">
-                LARGE FIRM. WHILE THERE HE WON SEVERAL AIA DESIGN AWARD FOR HIS FIRM.</p>
-
-            <p class="p3">
-                LATER, HE MOVED AGAIN WITH HIS FAMILY TO AUSTIN TEXAS WHERE HE SOON</p>
-
-            <p class="p1">
-                STARTED HIS OWN PRACTICE KNOWN AS BLOCK 16 ARCHITECTS AND URBANISTS. THAT</p>
-
-            <p class="p3">
-                FIRM MERGED WITH SKB ARCHITECTURE . HE HAS SINCE GONE BACK TO RESIDENTIAL DESIGN .</p>
-
-            <p class="p3">
-                MR. ALDERMAN HAS DESIGNED HUNDREDS OF RESIDENTIAL PROJECTS.</p>
-
-            <p class="p3">
-                HE CAN BRING TO BEAR HIS 45 PLUS YEARS OF EXPERIENCE IN ALL MANNER OF </p>
-
+            <p class="p1">STUART M. ALDERMAN ARCHITECT</p>
+            <p class="p1">HAS BEEN DESIGNING AND HONING THE CRAFT AND ART OF ARCHITECTURE </p>
+            <p class="p1">SINCE HE WAS ABOUT 10 YEARS OLD. HIS MOTHER WAS AN ARTIST AND HE</p>
+            <p class="p1">SPENT MUCH OF HIS TIME DRAWING, PAINTING AND CREATING AT HOME AND IN</p>
+            <p class="p2">ART CLASSES.</p>
+            <p class="p3">HE SPENT A SUMMER WHILE IN HIGH SCHOOL AT CORNELL UNIVERSITIES' </p>
+            <p class="p3">SUMMER PROGRAM IN ARCHITECTURE AND WENT ON TO GRADUATE FROM THE</p>
+            <p class="p1">SYRACUSE UNIVERSITY SCHOOL OF ARCHITECTURE</p>
+            <p class="p3">AFTER SEVERAL YEARS OF WORK IN DALLAS, TEXAS HE MOVED HIS FAMILY TO</p>
+            <p class="p3">UPSTATE NEW YORK AND HAD A CAREER AS THE PRINCIPAL DESIGNER FOR A</p>
+            <p class="p1">LARGE FIRM. WHILE THERE HE WON SEVERAL AIA DESIGN AWARD FOR HIS FIRM.</p>
+            <p class="p3">LATER, HE MOVED AGAIN WITH HIS FAMILY TO AUSTIN TEXAS WHERE HE SOON</p>
+            <p class="p1">STARTED HIS OWN PRACTICE KNOWN AS BLOCK 16 ARCHITECTS AND URBANISTS. THAT</p>
+            <p class="p3">FIRM MERGED WITH SKB ARCHITECTURE . HE HAS SINCE GONE BACK TO RESIDENTIAL DESIGN .</p>
+            <p class="p3">MR. ALDERMAN HAS DESIGNED HUNDREDS OF RESIDENTIAL PROJECTS.</p>
+            <p class="p3">HE CAN BRING TO BEAR HIS 45 PLUS YEARS OF EXPERIENCE IN ALL MANNER OF </p>
             <p class="p1">ARCHITECTURE AND DESIGN FOR YOUR PROJECT</p>
             <p class="p1">HE WORKS IN MANY STATES BUT PRINCIPALLY TEXAS, CALIFORNIA AND NEW YORK</p>
             <p class="p1">* &nbsp;DENOTES PROJECT DESIGNED BY STUART M. ALDERMAN ,AIA ARCHITECT WHILE A PRINCIPAL AT SKB </p>
@@ -115,14 +83,11 @@
             <p class="p4">TO EXPERIENCE DESIGN THAT ENHANCES YOUR HOME AND YOUR LIFE. </p>
             <p class="p4">I WANT TO YOU TO WAKE UP EVERY DAY AND BE GLAD YOU LIVE WHERE </p>
             <p class="p4">YOU DO. </p>
-
-
+            <p>&nbsp;</p>
         </div>
-
 
         <div class="col-2 g-0">
             <img id="stuartimage" src="./images/stuart.jpg" alt="FrontImage" width="100%" height="300">
-
         </div>
     </div>
 
